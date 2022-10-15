805bf126cd8960677d864f290edf8c752e0639f3
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2022-10-15 16:50:25 +0200

    Added BasicArm

---
+++ lab/esercizi/sett_4/44-45-rich-design/src/main/java/it/unibo/design/robot/api/BasicArm.java
@@ -0,0 +1,40 @@
+package it.unibo.design.robot.api;
+
+public class BasicArm {
+    private static final double MOVE_CONSUMPTION = 0.2;
+    private static final double GRAB_CONSUMPTION = 0.1;
+    private boolean grabbing;
+    private final String name;
+
+    public BasicArm(String name) {
+        this.name = name;
+        this.carriedItems = 0;
+    }
+    boolean isGrabbing() {
+        return grabbing;
+    }
+
+    public void pickUp() {
+        grabbing = true;
+    }
+
+    public void dropDown() {
+        grabbing = false;
+    }
+
+    /*public double getConsumption() {
+        return (grabbing ? GRAB_CONSUMPTION : 0) + MOVE_CONSUMPTION;        //Would have reduced the number of lines of code and methods.
+    }*/
+
+    public double getConsumptionForPickUp() {
+        return GRAB_CONSUMPTION + MOVE_CONSUMPTION;
+    }
+
+    public double getConsumptionForDropDown() {
+        return MOVE_CONSUMPTION;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
