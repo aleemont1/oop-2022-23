6c03dfdb5e6887a4cbf6470f53ca8da2fdc3eaaf
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2022-10-19 15:00:55 +0200

    Implemented WarehouseImpl and ProductImpl

---
+++ lab/esercizi/sett_5/53-implement-with-collections/src/main/java/it/unibo/collections/design/impl/ProductImpl.java
@@ -0,0 +1,23 @@
+package it.unibo.collections.design.impl;
+
+import it.unibo.collections.design.api.Product;
+
+public class ProductImpl implements Product {
+    private final String name;
+    private final double quantity;
+
+    public ProductImpl(final String name, final int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public double getQuantity() {
+        return this.quantity;
+    }
+
+}
